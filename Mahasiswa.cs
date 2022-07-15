185c179cfeed93a87e433e66ef84d245e9088bb3
irvanhidayat <101778631+irvanhidayat@users.noreply.github.com>
2022-07-15 10:37:24 +0700

    Add files via upload

---
+++ Mahasiswa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        public string nim { get; set; }
+        public string nama { get; set; }
+        public string Kelamin { get; set; }
+        public string total { get; set; }
+    }
+}
